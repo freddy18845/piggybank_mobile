d2acf0e2013639f99d1c69403a6536761ae5a581
Michael Twumasi <therealtwumasimichael@gmail.com>
2025-11-04 09:38:36 +0000

    settings page footer and background color

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:piggy_bank/widgets/footer.dart';
+
+import '../constant.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -10,6 +13,17 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: GRAY_COLOR,
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+            //for the footer
+            Footer(btnIndex: 2),
+          ],
+        ),
+      ),
+    );
   }
 }
